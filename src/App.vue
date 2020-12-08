603f1688a72703eeeab0d0eec3d8b0342f6299f0
Francisco Barron <franz.barron@outlook.com>
2020-12-08 16:41:05 -0600

    News in feed now added dynamically

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,22 @@
   <div id="app">
     <div class="row">
       <left-panel class="col col-3"></left-panel>
-      <router-view class="col col-9"/>
+      <router-view class="col col-9" />
     </div>
   </div>
 </template>
 
 <script>
-import LeftPanel from './components/LeftPanel.vue'
+import LeftPanel from './components/LeftPanel.vue';
 
 export default {
   name: 'App',
-  components: { LeftPanel}
-}
+  components: { LeftPanel }
+};
 </script>
 
 <style>
+#app {
+  overflow-x: hidden;
+}
 </style>
