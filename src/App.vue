70162a327a532ecfa3a51edb41341fa0ffe515e0
Francisco Barron <franz.barron@outlook.com>
2020-12-08 01:36:55 -0600

    Add Carousel for most important news

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div class="row">
-      <left-panel class="col col-4"></left-panel>
-      <router-view class="col col-8"/>
+      <left-panel class="col col-3"></left-panel>
+      <router-view class="col col-9"/>
     </div>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,68 @@
 <template>
   <div class="home">
     <div class="row">
-      <center-panel class="col"></center-panel>
-      <right-panel class="col"></right-panel>
+      <div class="col col-8">
+        <!-- Most important news -->
+        <b-carousel
+          id="carousel-1"
+          v-model="slide"
+          :interval="4000"
+          controls
+          indicators
+          background="#ababab"
+          img-width="1024"
+          img-height="480"
+          style="text-shadow: 1px 1px 2px #333;"
+          @sliding-start="onSlideStart"
+          @sliding-end="onSlideEnd"
+          label-prev=""
+          label-next=""
+          no-animation="true"
+        >
+          <!-- Text slides with image -->
+          <b-carousel-slide
+            caption="First slide"
+            text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+            img-src="https://picsum.photos/1024/480/?image=52"
+          ></b-carousel-slide>
+
+          <!-- Slides with custom text -->
+          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+            <h1>Hello world!</h1>
+          </b-carousel-slide>
+
+          <!-- Slides with image only -->
+          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+          <!-- Slides with img slot -->
+          <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+          <b-carousel-slide>
+            <template #img>
+              <img
+                class="d-block img-fluid w-100"
+                width="1024"
+                height="480"
+                src="https://picsum.photos/1024/480/?image=55"
+                alt="image slot"
+              >
+            </template>
+          </b-carousel-slide>
+
+          <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+          <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+              a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+            </p>
+          </b-carousel-slide>
+        </b-carousel>
+      </div>
+      <right-panel class="col col-4"></right-panel>
     </div>
   </div>
 </template>
 
 <script>
-import CenterPanel from '../components/CenterPanel.vue'
 import RightPanel from '../components/RightPanel.vue'
 // @ is an alias to /src
 // import CenterPanel from '@/components/CenterPanel.vue'
@@ -17,7 +71,6 @@ import RightPanel from '../components/RightPanel.vue'
 export default {
   name: 'Home',
   components: {
-    CenterPanel,
     RightPanel
   }
 }
