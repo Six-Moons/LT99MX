8435493e76f005a5f3e292441e23c2e935e0c005
HumbertoGlez <hglez.98s@gmail.com>
2020-12-08 00:03:53 -0600

    Added menu hamburguesa owo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div id="app">
+   <nav class="main-nav">
+     <Burger></Burger>
+     <div class="logo">
+       <img class="logo-img" src="./assets/logo.png">
+     </div>
+   </nav>
+
+   <Sidebar>
+     <ul class="sidebar-panel-nav">
+       <li><a href="#home">Home</a></li>
+       <li><a href="#about">About</a></li>
+       <li><a href="#contact">Contact</a></li>
+     </ul>
+   </Sidebar>
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Burger from './components/menu/Burger.vue';
+import Sidebar from './components/menu/Sidebar.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ name: 'app',
+ components: {
+   Burger,
+   Sidebar
+ }
 }
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+   height: 100%;
+   overflow:hidden;
+ }
+
+ body {
+   border: 0; margin: 0; padding: 0;
+   font-family: 'Lato';
+   height: 100%;
+   background: rgb(101,31,87);
+   background: linear-gradient(45deg, rgba(101,31,87,1) 0%, rgba(225,113,87,1) 48%, rgba(249,248,113,1) 100%);
+ }
+
+ .logo {
+   align-self: center;
+   color: #fff;
+   font-weight: bold;
+   font-family: 'Lato'
+ }
+
+ .main-nav {
+   display: flex;
+   justify-content: space-between;
+   padding: 0.5rem 0.8rem;
+ }
+
+ ul.sidebar-panel-nav {
+   list-style-type: none;
+ }
+
+ ul.sidebar-panel-nav > li > a {
+   color: #fff;
+   text-decoration: none;
+   font-size: 1.5rem;
+   display: block;
+   padding-bottom: 0.5em;
+ }
+
+.logo-img {
+    max-height: 50px;
 }
+
 </style>
