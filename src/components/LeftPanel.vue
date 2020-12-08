b3f757872a21d1984c66c1b548400429e43747a4
HumbertoGlez <hglez.98s@gmail.com>
2020-12-08 04:45:11 -0600

    Left panel base design and layout

--- src/components/LeftPanel.vue
+++ src/components/LeftPanel.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="left-panel">
-        <ul class="panel-nav">
-            <li><a href="#Home">Tetris</a></li>
+        <ul class="nav-sidebar list-unstyled panel-nav">
+            <li class="active"><a href="#Home">Tetris</a></li>
             <li><a href="#Rankings">Rankings</a></li>
             <li><a href="#Noticias">Noticias</a></li>
-            <li><a href="#MiCuenta">Mi Cuenta</a></li>
             <li><a href="#FAQ">FAQ / Comentarios</a></li>
             <li><a href="#Donaciones">Donaciones</a></li>
-            <li><a class="btn btn-danger logout" href="#Logout">Cerrar Sesión</a></li>
+            <li><a href="#MiCuenta">Mi Cuenta</a></li>
+        </ul>
+        <ul class="logout">
+            <li><a class="btn btn-lg btn-danger" href="#Logout">Cerrar Sesión</a></li>
         </ul>
     </div>
 </template>
@@ -20,10 +22,15 @@ export default {
 
 <style scoped>
     .left-panel{
+        font-family: "HomepageBaukasten-bold";
         height: 100vh;
-        background-color: rgb(17, 30, 73);
+        background-color:rgb(12, 9, 58);
+        transition: all 0.3s;
     }
     ul.panel-nav {
+        list-style-type: none;
+    }
+    ul.logout {
         list-style-type: none;
     }
     ul.panel-nav > li > a {
@@ -32,11 +39,33 @@ export default {
         font-size: 1.5rem;
         display: block;
         padding-bottom: 0.5em;
+        padding-left: 30px;
     }
+    .panel-nav > .active > a,
+    .panel-nav > .active > a:hover,
+    .panel-nav > .active > a:focus {
+    color: rgb(223, 223, 223);
+    background-color: rgb(8, 6, 20);
+    }
+    .panel-nav > li > a:hover{
+        color: rgb(12, 9, 58);
+        background: #fff;
+    }
+    a, a:hover, a:focus {
+        color: inherit;
+        text-decoration: none;
+        transition: all 0.3s;
+    }
+
     .panel-nav {
         padding-top: 5%;
     }
     .logout {
-        margin-right: 10%;
+        position: absolute;
+        display: inline-block;
+        bottom: 0;
+        font-size: 1.5rem;
+        color: #fff;
+        width: 100%;
     }
 </style>
--- src/components/RightPanel.vue
+++ src/components/RightPanel.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>Right Panel</p>
+    <div class="right-panel">
+        <p>Right Panel</p>
+    </div>
 </template>
 
 <script>
@@ -7,3 +9,11 @@ export default {
     name: 'RightPanel'
 }
 </script>
+<style scoped>
+.right-panel{
+    background: rgba(0,212,255,1);
+    font-family: "HomepageBaukasten-bold";
+    height: 100vh;
+    transition: all 0.3s;
+}
+</style>
