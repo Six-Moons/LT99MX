0d0c4824c645379722dbb8a88bfc4226e6150199
albgv1402 <albertogarv1402@gmail.com>
2020-12-11 15:52:22 -0600

    Connection to postgres working

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+// });
 
-app.listen(8080, () => {
-  console.log('Server listening on http://localhost:8080');
-});
+// app.listen(8080, () => {
+//   console.log('Server listening on http://localhost:8080');
+// });
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
