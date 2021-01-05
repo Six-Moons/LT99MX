219977a9070fb4e9d502e694a5de2342e311003e
albgv1402 <albertogarv1402@gmail.com>
2021-01-04 18:16:13 -0600

    Estandarización de respuestas del servidor

--- queries/usuarios.js
+++ queries/usuarios.js
@@ -7,11 +7,21 @@ let {SECRET_KEY, SALT_ROUNDS} = require('./../config');
 const tablaUsuarios = 'usuarios'
 const infoUsuario = 'userName, nombre, estado, foto, descripcion, insigniaFavorita';
 
+function respuesta(res, mensaje, codigo, data) {
+	res.statusMessage = mensaje;
+	return res.status(codigo).json(data);
+}
+
+function mensajeDeError(res, err, errMsg, msg, codigo) {
+	console.log(errMsg, err);
+	return valueToReturn(res, msg, codigo, {status: codigo, message: msg});
+}
+
 const conseguirUsuarios = (request, response) => {
   
     pool.query(`SELECT ${infoUsuario} FROM ${tablaUsuarios} ORDER BY userID ASC`, (error, results) => {
         if (error) {
-            throw error;
+            return mensajeDeError(response, error, error.detail, error.detail, 408);
         }
         response.status(200).json(results.rows)
     })
@@ -22,7 +32,7 @@ const conseguirUsuarioPorUsername = (request, response) => {
     console.log(username);
     pool.query(`SELECT ${infoUsuario} FROM ${tablaUsuarios} WHERE username = $1`, [username], (error, results) => {
         if (error) {
-            throw error
+            return mensajeDeError(response, error, error.detail, error.detail, 408);
         }
         response.status(200).json(results.rows[0])
     })
@@ -32,12 +42,8 @@ const login = (request, response) => {
     const {username, correo, contrasenia} = request.body
 
     if ((!username || !correo) && !contrasenia) {
-        response.statusMessage = 'Falta campo';
-        let statusCode = 400;
-        return response.status( statusCode ).json({
-            status : statusCode,
-            message : response.statusMessage
-        })
+        let msg = 'Falta campo en el cuerpo';
+        return mensajeDeError(response, error, msg, msg, 400);
     }
 
     let loginUsuario = ((username) ? 'username' : 'correo')
@@ -46,13 +52,7 @@ const login = (request, response) => {
     pool.query(`SELECT * FROM ${tablaUsuarios} WHERE ${loginUsuario} = $1`, [varLogin], 
     (error, results) => {
         if (error) {
-            console.log(error);
-            response.statusMessage = error.detail;
-            let statusCode = 408;
-            return response.status( statusCode ).json({
-                status : statusCode,
-                message : response.statusMessage
-            })
+            return mensajeDeError(response, error, error.detail, error.detail, 408);
         } else if (results.rows.length) {
             bcrypt.compare(contrasenia, results.rows[0].contrasenia, function(err, result) {
                 if (result) {
@@ -60,15 +60,8 @@ const login = (request, response) => {
                     let user = {username, soiadmin, nombre, estado, insigniafavorita};
                     jwt.sign({user}, SECRET_KEY, (error, token) => {
                         if (error) {
-                            console.log(error);
-                            // response.statusMessage = error;
-                            response.statusMessage = 'Error con JWT';
-                            let statusCode = 408;
-                            return response.status( statusCode ).json({
-                                status: statusCode,
-                                message: response.statusMessage,
-                                error
-                            })
+                            let msg = 'Error con JWT';
+                            return mensajeDeError(response, error, msg, msg, 408);
                         } else {
                             response.status(200).json({
                                 message: `Login correcto de: ${varLogin}`,
@@ -109,13 +102,7 @@ const crearUsuario = (request, response) => {
         pool.query(`INSERT INTO ${tablaUsuarios} (${camposTablaUsuarios}) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [userID, soiAdmin, username, correo, contraseniaEncriptada, nombre, estado, fotoPath, descripcion], 
         (error, results) => {
             if (error) {
-                console.log(error);
-                response.statusMessage = error.detail;
-                let statusCode = 408;
-                return response.status( statusCode ).json({
-                    status : statusCode,
-                    message : response.statusMessage
-                })
+                return mensajeDeError(response, error, error.detail, error.detail, 408);
             }
             response.status(201).send(`User added with username: ${username}`)
         });
@@ -128,25 +115,32 @@ const actualizarUsuario = (request, response) => {
         if(err) {
             response.sendStatus(403);
         } else {
-            response.status(200).json({
-                message: `User modified with username`,
-                authData
-            });
+            const username = parseInt(request.params.username)
+            const {nombre, estado, foto, descripcion} = request.body
+        
+            pool.query(
+                `UPDATE ${tablaUsuarios} SET nombre = $1, estado = $2, foto = $3, descripcion = $4 WHERE username = $5`,
+                [nombre, estado, foto, descripcion, username],
+                (error, results) => {
+                    if (error) {
+                        throw error
+                    } else {
+                        const {username, soiadmin, nombre, estado, insigniafavorita} = results.rows[0];
+                        let user = {username, soiadmin, nombre, estado, insigniafavorita};
+                        jwt.sign({user}, SECRET_KEY, (error, token) => {
+                            if (error) {
+                                let msg = 'Error con JWT'
+                                return mensajeDeError(response, error, msg, msg, 408);
+                            } else {
+                                let msg = `Se actualizó la información correctamente de ${username}`
+                                return respuesta(response, msg, 200, {msg, token});
+                            }
+                        });
+                    }
+                }
+            )
         }
     });
-    // const username = parseInt(request.params.username)
-    // const {nombre, estado, foto, descripcion} = request.body
-
-    // pool.query(
-    //     `UPDATE ${tablaUsuarios} SET nombre = $1, estado = $2, foto = $3, descripcion = $4 WHERE username = $5`,
-    //     [nombre, estado, foto, descripcion, username],
-    //     (error, results) => {
-    //         if (error) {
-    //             throw error
-    //         }
-    //         response.status(200).send(`User modified with username: ${username}`)
-    //     }
-    // )
 }
 
 const borrarUsuario = (request, response) => {
